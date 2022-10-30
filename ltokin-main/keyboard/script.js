a0863a3c07197804d59bca8839c62a213d145830
LucaMontagnaa <101721205+LucaMontagnaa@users.noreply.github.com>
2022-10-30 20:57:50 -0300

    changes.2

--- ltokin-main/keyboard/script.js
+++ ltokin-main/keyboard/script.js
@@ -1,20 +1,10 @@
-//#region 
-window.addEventListener("load", () => {
-    webgazer.setGazeListener(function (data, elapsedTime) {
-        if (data == null) {
-            return;
-        }
-        var xprediction = data.x; //these x coordinates are relative to the viewport
-        var yprediction = data.y; //these y coordinates are relative to the viewport
-        console.log(xprediction, yprediction);
-    }).begin();
+//#region
 
-    setInterval(async () => {
-        let data = await webgazer.getCurrentPrediction();
-        console.log(data);
-    }, 1000);
-});
-//#endregion
+//var elemento = document.getElementById("btnA");
+//var posicion = elemento.getBoundingClientRect();
+
+//console.log(posicion.top, posicion.right, posicion.bottom, posicion.left);
+
 //Botones, Text Areas, etc.
 let onButton
 let texto = document.getElementById ("textArea")
